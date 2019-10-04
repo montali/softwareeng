c9d8bf1d53742e383ac1bcb0dcbd03426216971b
tritacibo <filippo.botti.itis@gmail.com>
2019-10-04 17:16:12 +0200

    Complete comments on the class Person

--- esercitazione1/src/main/java/it/unipr/bottimontali/Person.java
+++ esercitazione1/src/main/java/it/unipr/bottimontali/Person.java
@@ -3,8 +3,6 @@ package it.unipr.bottimontali;
 /**
  * Person is in charge of saving a person object with its properties.
  * Each person has a name, a surname, an email and a password.
- * Each person has two constructors, and get and set methods for every private variable.
- * Each person has one behavior for the registration to the courses. This behavior accept two arguments: the first is a Club object and the second is an Activity object. 
  *  
  * TODO: Encrypt the password
  * 
@@ -35,49 +33,99 @@ public class Person
         this.email = email;
         this.password = password;
     }
-
+    /** 
+     * This behavior gets the Person's name.
+     *
+     * @return String the Person's name.
+     * @since           1.0
+     */
     public String getName() {
         return this.name;
     }
-
+    /** 
+     * This behavior sets the Person's name.
+     * @param String name for the Person.
+     * @since           1.0
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /** 
+     * This behavior gets the Person's surname.
+     *
+     * @return String the Person's surname.
+     * @since           1.0
+     */
     public String getSurname() {
         return this.surname;
     }
-
+    /** 
+     * This behavior sets the Person's surname.
+     * @param String surname for the Person.
+     * @since           1.0
+     */
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
+    /** 
+     * This behavior gets the Person's email.
+     *
+     * @return String the Person's email.
+     * @since           1.0
+     */
     public String getEmail() {
         return this.email;
     }
-
+    /** 
+     * This behavior sets the Person's email.
+     * @param String email for the Person.
+     * @since           1.0
+     */
     public void setEmail(String email) {
         this.email = email;
     }
-
+    /** 
+     * This behavior gets the Person's password.
+     *
+     * @return String the Person's password.
+     * @since           1.0
+     */
     public String getPassword() {
         return this.password;
     }
-
+    /** 
+     * This behavior sets the Person's password.
+     * @param String password for the Person.
+     * @since           1.0
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    /**
+     * This behavior subscribes a Person to an activity.
+     * This behavior is a wrapper for the Club's subscription behavior.
+     * @param Club club
+     * @param Activity desiredActivity
+     */
     public void subscribeToActivity(Club club, Activity desiredActivity)
     {
         club.subscribeToActivity(this, desiredActivity);
     }
-
+    /**
+     * This behavior deletes a Person to an activity.
+     * This behavior is a wrapper for the Club's unsubscription behavior.
+     * @param Club club
+     * @param Activity unsubscribingActivity
+     */
     public void unsubscribeFromActivity(Club club, Activity unsubscribingActivity)
     {
         club.unSubscribeFromActivity(this, unsubscribingActivity);
     }
-
+    /**
+     * Behavior that verifies the equality of two person.
+     * @param Object o
+     * @return boolean 
+     */
     @Override
     public boolean equals(Object o) {
         if (o == this)
@@ -88,7 +136,10 @@ public class Person
         Person person = (Person) o;
         return (name == person.name) && (surname == person.surname) && (email == person.email) && (password == person.password);
     }
-
+    /**
+     * This behavior print a complete person's description.
+     * 
+     */
     @Override
     public String toString() {
         return "{" +
